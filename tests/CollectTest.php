e7e52670e2459e7bbbd5ae5cc0567aac6a625f2a
F1reN1kto <maksimcymbal77@gmail.com>
2025-03-07 16:33:43 +0700

    Сделал тест метода push

--- tests/CollectTest.php
+++ tests/CollectTest.php
@@ -1,13 +1,22 @@
 <?php
-
+use Collect\Collect;
 use PHPUnit\Framework\TestCase;
 
 class CollectTest extends TestCase
 {
-    public function testCount()
+    private $collect;
+    protected function setUp(): void
     {
-        $collect = new Collect\Collect([13,17]);
-        $this->assertSame(2, $collect->count());
+        $this->collect = new Collect();
     }
+    // Тест для метода push()
+    public function testPush()
+    {
+        $this->collect->push(1);
+        $this->collect->push(2);
+        $this->collect->push(3);
+        $this->collect->push(4);
 
+        $this->assertEquals([1, 2, 3, 4], $this->collect->toArray());
+    }
 }
