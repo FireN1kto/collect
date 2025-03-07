ef1aea60a7cb08b0a8f8eb6507d912255a0399c2
F1reN1kto <maksimcymbal77@gmail.com>
2025-03-07 17:08:50 +0700

    Сделал тест метода unshift()

--- tests/CollectTest.php
+++ tests/CollectTest.php
@@ -9,14 +9,14 @@ class CollectTest extends TestCase
     {
         $this->collect = new Collect();
     }
-    // Тест для метода push()
-    public function testPush()
-    {
-        $this->collect->push(1);
-        $this->collect->push(2);
-        $this->collect->push(3);
-        $this->collect->push(4);
 
-        $this->assertEquals([1, 2, 3, 4], $this->collect->toArray());
+    public function testUnshift()
+    {
+        $this->collect->unshift(3);
+        $this->collect->unshift(2);
+        $this->collect->unshift(1);
+        $this->collect->unshift(0);
+
+        $this->assertEquals([0, 1, 2, 3], $this->collect->toArray());
     }
 }
